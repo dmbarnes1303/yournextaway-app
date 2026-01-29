f9ce7817f9ef5d8256da85b61e5581d5b8826b67
dmbarnes1303 <d.m.barnes1303@gmail.com>
2026-01-29 13:31:44 +0000

    Create cityTeams.ts

---
+++ src/constants/cityTeams.ts
@@ -0,0 +1,16 @@
+export type CityTeamsMap = Record<string, string[]>;
+
+export const CITY_TEAMS: CityTeamsMap = {
+  london: [
+    "arsenal",
+    "chelsea",
+    "tottenham-hotspur",
+    "west-ham-united",
+    "crystal-palace",
+    "brentford"
+  ],
+  manchester: [
+    "manchester-united",
+    "manchester-city"
+  ],
+};
