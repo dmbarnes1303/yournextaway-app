e30a2a611d98dc1722d4c48d67432318a1bf153f
dmbarnes1303 <d.m.barnes1303@gmail.com>
2026-02-06 17:48:06 +0000

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,35 @@
 // app/_layout.tsx
 import "@/src/utils/errorLogger";
-import { Stack } from "expo-router";
 import React from "react";
+import { Stack } from "expo-router";
+
+import { ProProvider } from "@/src/context/ProContext";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" />
-      <Stack.Screen name="trip/[id]" />
-      <Stack.Screen name="trip/build" />
-      <Stack.Screen name="fixture/[id]" />
-      <Stack.Screen name="city/[slug]" />
-      <Stack.Screen name="city/[cityKey]" />
-      <Stack.Screen name="team/[teamKey]" />
-      <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-    </Stack>
+    <ProProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+
+        {/* Core flows */}
+        <Stack.Screen name="match/[id]" />
+        <Stack.Screen name="trip/[id]" />
+        <Stack.Screen name="trip/build" />
+
+        {/* Guides */}
+        <Stack.Screen name="city/[slug]" />
+        <Stack.Screen name="team/[teamKey]" />
+
+        {/* Modal */}
+        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+
+        {/**
+         * NOTE:
+         * - Removed "fixture/[id]" because your app navigates to /match/[id].
+         * - Removed "city/[cityKey]" because it conflicts with city/[slug].
+         * If those files still exist, we should delete/rename them properly in the tree.
+         */}
+      </Stack>
+    </ProProvider>
   );
 }
