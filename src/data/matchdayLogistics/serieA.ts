ad9ef2a7d5b6f776111eba9643f8c8c11dbff71d
dmbarnes1303 <d.m.barnes1303@gmail.com>
2026-02-23 17:48:20 +0000

    Create serieA.ts

---
+++ src/data/matchdayLogistics/serieA.ts
@@ -0,0 +1,5 @@
+// src/data/matchdayLogistics/serieA.ts
+import type { MatchdayLogistics } from "./types";
+
+const serieALogistics: Record<string, MatchdayLogistics> = {};
+export default serieALogistics;
