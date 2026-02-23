6ac046b7e6d93665683f2b06e760a5aebf1ae8cb
dmbarnes1303 <d.m.barnes1303@gmail.com>
2026-02-23 17:49:00 +0000

    Create ligue1.ts

---
+++ src/data/matchdayLogistics/ligue1.ts
@@ -0,0 +1,5 @@
+// src/data/matchdayLogistics/ligue1.ts
+import type { MatchdayLogistics } from "./types";
+
+const ligue1Logistics: Record<string, MatchdayLogistics> = {};
+export default ligue1Logistics;
