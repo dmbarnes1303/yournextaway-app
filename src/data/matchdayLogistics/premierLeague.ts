86d772ed85e258c70fe6686a815cf3def20e4046
dmbarnes1303 <d.m.barnes1303@gmail.com>
2026-02-23 17:46:44 +0000

    Create premierLeague.ts

---
+++ src/data/matchdayLogistics/premierLeague.ts
@@ -0,0 +1,5 @@
+// src/data/matchdayLogistics/premierLeague.ts
+import type { MatchdayLogistics } from "./types";
+
+const premierLeagueLogistics: Record<string, MatchdayLogistics> = {};
+export default premierLeagueLogistics;
