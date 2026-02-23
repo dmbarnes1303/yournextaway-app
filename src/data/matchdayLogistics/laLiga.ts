5625cb2386efbab746bd6de2af13ebd0ed1fa839
dmbarnes1303 <d.m.barnes1303@gmail.com>
2026-02-23 17:47:23 +0000

    Create laLiga.ts

---
+++ src/data/matchdayLogistics/laLiga.ts
@@ -0,0 +1,5 @@
+// src/data/matchdayLogistics/laLiga.ts
+import type { MatchdayLogistics } from "./types";
+
+const laLigaLogistics: Record<string, MatchdayLogistics> = {};
+export default laLigaLogistics;
