738f5ced937b625cf1d022bac570721ae244a0af
dmbarnes1303 <d.m.barnes1303@gmail.com>
2026-03-08 15:49:55 +0000

    Update index.ts

--- src/data/stadiums/index.ts
+++ src/data/stadiums/index.ts
@@ -24,6 +24,7 @@ import bestaDeildStadiums from "./bestaDeild";
 import nbIStadiums from "./nbI";
 import superLigaStadiums from "./superLiga";
 import hnlStadiums from "./hnl";
+import superLigaSerbiaStadiums from "./superLigaSerbia";
 
 export const stadiums: Record<string, StadiumRecord> = {
   ...premierLeagueStadiums,
@@ -49,6 +50,7 @@ export const stadiums: Record<string, StadiumRecord> = {
   ...nbIStadiums,
   ...superLigaStadiums,
   ...hnlStadiums,
+  ...superLigaSerbiaStadiums,
 };
 
 export function getStadium(stadiumKey: string): StadiumRecord | null {
