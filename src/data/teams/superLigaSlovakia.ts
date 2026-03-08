0a3f57ed0fbcd84fbe2fe0696e50b16c5e08214d
dmbarnes1303 <d.m.barnes1303@gmail.com>
2026-03-08 15:52:14 +0000

    Create superLigaSlovakia.ts

---
+++ src/data/teams/superLigaSlovakia.ts
@@ -0,0 +1,105 @@
+import type { TeamRecord } from "./types";
+
+const SLOVAKIA = "Slovakia";
+
+export const superLigaSlovakiaTeams: Record<string, TeamRecord> = {
+
+  "slovan-bratislava": {
+    teamKey: "slovan-bratislava",
+    name: "Slovan Bratislava",
+    city: "Bratislava",
+    country: SLOVAKIA,
+    stadiumKey: "tehelne-pole",
+  },
+
+  "dunajska-streda": {
+    teamKey: "dunajska-streda",
+    name: "DAC Dunajská Streda",
+    city: "Dunajská Streda",
+    country: SLOVAKIA,
+    stadiumKey: "mol-arena",
+  },
+
+  "zilina": {
+    teamKey: "zilina",
+    name: "MŠK Žilina",
+    city: "Žilina",
+    country: SLOVAKIA,
+    stadiumKey: "stadion-pod-dubnom",
+  },
+
+  "spartak-trnava": {
+    teamKey: "spartak-trnava",
+    name: "Spartak Trnava",
+    city: "Trnava",
+    country: SLOVAKIA,
+    stadiumKey: "anton-malatinseho-stadium",
+  },
+
+  "podbrezova": {
+    teamKey: "podbrezova",
+    name: "FK Železiarne Podbrezová",
+    city: "Podbrezová",
+    country: SLOVAKIA,
+    stadiumKey: "zelpo-arena",
+  },
+
+  "michalovce": {
+    teamKey: "michalovce",
+    name: "MFK Zemplín Michalovce",
+    city: "Michalovce",
+    country: SLOVAKIA,
+    stadiumKey: "mestsky-futbalovy-stadion-michalovce",
+  },
+
+  "fc-kosice": {
+    teamKey: "fc-kosice",
+    name: "FC Košice",
+    city: "Košice",
+    country: SLOVAKIA,
+    stadiumKey: "kosicka-futbalova-arena",
+  },
+
+  "komarno": {
+    teamKey: "komarno",
+    name: "KFC Komárno",
+    city: "Komárno",
+    country: SLOVAKIA,
+    stadiumKey: "stadion-komarno",
+  },
+
+  "ruzomberok": {
+    teamKey: "ruzomberok",
+    name: "MFK Ružomberok",
+    city: "Ružomberok",
+    country: SLOVAKIA,
+    stadiumKey: "stadion-mfk-ruzomberok",
+  },
+
+  "trencin": {
+    teamKey: "trencin",
+    name: "AS Trenčín",
+    city: "Trenčín",
+    country: SLOVAKIA,
+    stadiumKey: "sihoť-stadium",
+  },
+
+  "tatran-presov": {
+    teamKey: "tatran-presov",
+    name: "Tatran Prešov",
+    city: "Prešov",
+    country: SLOVAKIA,
+    stadiumKey: "futbal-tatran-arena",
+  },
+
+  "skalica": {
+    teamKey: "skalica",
+    name: "MFK Skalica",
+    city: "Skalica",
+    country: SLOVAKIA,
+    stadiumKey: "mestsky-stadion-skalica",
+  },
+
+};
+
+export default superLigaSlovakiaTeams;
