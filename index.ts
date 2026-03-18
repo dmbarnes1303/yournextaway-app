6ad5fdf765b1728f3fe654d7fa61d2e883e63561
dmbarnes1303 <d.m.barnes1303@gmail.com>
2026-03-18 21:28:01 +0000

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,1 @@
-import { Platform } from "react-native";
-
-async function bootstrap() {
-  if (Platform.OS !== "web") {
-    await import("./utils/errorLogger");
-  }
-
-  await import("expo-router/entry");
-}
-
-void bootstrap();
+import "expo-router/entry";
